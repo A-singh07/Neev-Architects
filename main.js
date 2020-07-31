fe0c57e2d7ea28df01e60559767139866d61419c
Aman Singh <imaman07singh@gmail.com>
2020-07-31 08:51:40 +0530

    very initial

--- main.js
+++ main.js
@@ -5,8 +5,8 @@
 
 // to open the top-nav
 function openNav() {
-    document.getElementById('top-nav').style.height = "calc(40vh + 60px)";
-    document.getElementById('main').style.marginTop = "40vh";
+    document.getElementById('top-nav').style.height = "calc(30vh + 60px)";
+    document.getElementById('main').style.marginTop = "30vh";
     document.getElementById('close-btn').style.display = "block";
 }
 
@@ -16,3 +16,27 @@ function closeNav() {
     document.getElementById('main').style.marginTop = "0";
     document.getElementById('close-btn').style.display = "none";
 }
+
+
+
+/* ----- Media Query ------ */
+// for service-mobile
+/*
+function myMedia() {
+    let y = document.getElementsByClassName('serv-mob')
+    if (x.matches) {
+        for (let i = 0; i < y.length; i++) {
+            y[i].style.display = "none"
+        }
+    }
+    else {
+        for (let i = 0; i < y.length; i++) {
+            y[i].style.display = "block"
+        }
+    }
+}
+var x = window.matchMedia("(min-width: 900px)")
+myMedia()
+x.addListener(myMedia)
+*/
+
