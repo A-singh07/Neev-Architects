19fde155ffab7e0ee4fbdee8322e84ab636dd5ef
Aman Singh <imaman07singh@gmail.com>
2020-08-02 00:05:02 +0530

    services done

--- main.js
+++ main.js
@@ -5,38 +5,47 @@
 
 // to open the top-nav
 function openNav() {
-    document.getElementById('top-nav').style.height = "calc(30vh + 60px)";
-    document.getElementById('main').style.marginTop = "30vh";
+    document.getElementById('top-nav').style.height = "calc(50vh)";
     document.getElementById('close-btn').style.display = "block";
 }
 
 // to close the top-nav
 function closeNav() {
     document.getElementById('top-nav').style.height = "0";
-    document.getElementById('main').style.marginTop = "0";
     document.getElementById('close-btn').style.display = "none";
 }
 
+// For Services
+function services_media() {
+    var img = document.getElementsByClassName('serv-img');
 
-
-/* ----- Media Query ------ */
-// for service-mobile
-/*
-function myMedia() {
-    let y = document.getElementsByClassName('serv-mob')
     if (x.matches) {
-        for (let i = 0; i < y.length; i++) {
-            y[i].style.display = "none"
+        document.getElementById("ser-container-desk").classList.add("ser-container-desk");
+
+        for (let i = 0; i < img.length; i++) {
+            img[i].style.display = "block";
         }
     }
     else {
-        for (let i = 0; i < y.length; i++) {
-            y[i].style.display = "block"
+        document.getElementById("ser-container-desk").classList.remove("ser-container-desk");
+
+        for (let i = 0; i < img.length; i++) {
+            img[i].style.display = "none";
         }
     }
 }
-var x = window.matchMedia("(min-width: 900px)")
-myMedia()
-x.addListener(myMedia)
-*/
+var x = window.matchMedia("(min-width:900px)")
+services_media()
+x.addListener(services_media)
 
+
+// About for Desktop
+
+for (let i = 0; i < wh.length; i++) {
+    document.getElementsByClassName('wh')[i].addEventListener("click", abtDetail);
+}
+
+function abtDetail() {
+    document.getElementById('abt-detail-container').style.display = "block";
+
+}
