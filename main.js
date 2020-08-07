bd994ab560c020df27c4d4b62767ae22402f6cde
Aman Singh <imaman07singh@gmail.com>
2020-08-07 15:52:56 +0530

    tabs in prj & abt

--- main.js
+++ main.js
@@ -41,22 +41,55 @@ x.addListener(services_media)
 
 
 // Projects for desktop
-function residency() {
-    document.getElementById("resd-1").style.display = "none";
+function openContent(type) {
+
+    var tabContent = document.getElementsByClassName("detail-content");
+    var tabLink = document.getElementsByClassName("tablink");
+    let i;
+
+    // Hidding detail-content
+    for (i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
+    }
+    // display the the clicked tab-content
+    document.getElementById(type).style.display = 'block';
+
+    //Hiding active class
+    for (i = 0; i < tabLink.length; i++) {
+        tabLink[i].className = tabLink[i].className.replace(" active", "");
+    }
+    // Seting active for current/selected tab
+    event.currentTarget.className += " active"; // notice the space before 'active', it's to overwriting the existing values
 }
-
-
-
+// default Open tab contents
+document.getElementById('residency').style.display = "block";
+// document.getElementById('defaultOpen').className += " active";
 
 
 
 // About for Desktop
+function aboutFunc(wh_abt) {
 
-function whtFunc() {
-    document.getElementById('what').classList.toggle("show");
+    var abtTab = document.getElementsByClassName('abtTab');
+    var abtDetail = document.getElementsByClassName('abt-detail');
+    let i;
+
+    // hiding abt-detail
+    for (i = 0; i < abtDetail.length; i++) {
+        abtDetail[i].style.display = "none";
+    }
+
+    //display the clicked content
+    document.getElementById(wh_abt).style.display = "block";
+
+    // Hiding abt_active class
+    for (i = 0; i < abtTab.length; i++) {
+        abtTab[i].className = abtTab[i].className.replace(" abtActive", "");
+    }
+    // Setting abtActive for current/selected tab
+    event.currentTarget.className += " abtActive";
+
 }
-
-// function whoFunc() {
-//     document.getElementById('who').style.display = "block";
-//     document.getElementById('what').style.display = "none";
-// }
+// default Open tab contents
+// document.getElementById('what').style.display = "block";
+// document.getElementById('whatID').className += " abtActive";
