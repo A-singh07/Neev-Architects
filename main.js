826f89e53635476945b4b8a875f363c58a042f31
Aman Singh <imaman07singh@gmail.com>
2020-09-29 00:05:21 +0530

    Final

--- main.js
+++ main.js
@@ -1,11 +1,35 @@
-// Loader
-// function loaderTime() {
-//     setTimeout(showpage, 1000);
-// }
-// function showpage() {
-//     document.getElementById('loader').style.display = "none";
-//     document.getElementById('mother-container').style.display = "block";
-// }
+//Loader
+
+function loaderTime() {
+    setTimeout(showpage, 500);
+}
+function showpage() {
+    document.getElementById('loader').style.display = "none";
+    document.getElementById('mother-container').style.display = "block";
+}
+
+
+// SplitScreen for Desktop
+document.addEventListener('DOMContentLoaded', function () {
+    let wrapper = document.getElementById('wrapper');
+    let topLayer = wrapper.querySelector('.top');
+    let handle = wrapper.querySelector('.handle');
+    let skew = 0;
+    let delta = 0;
+
+    if (wrapper.className.indexOf('skewed') != -1) {
+        skew = 1000;
+    }
+
+    wrapper.addEventListener('mousemove', function (e) {
+        delta = (e.clientX - window.innerWidth / 2) * 0.5;
+
+        handle.style.left = e.clientX + delta + 'px';
+
+        topLayer.style.width = e.clientX + skew + delta + 'px';
+    });
+});
+
 
 
 
@@ -47,9 +71,7 @@ services_media()
 x.addListener(services_media)
 
 
-
 // Projects Section for desktop
-
 
 
 //Assign the ID for the slide-container
@@ -165,10 +187,6 @@ function slider() {
 
     slideshow();
 }
-
-
-
-
 
 
 // About for Desktop
